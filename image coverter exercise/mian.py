33513126fccbcfb53b8dd3674deff36fa56698ea
MohammadHosseinMinaee <minaie1381@gmail.com>
2024-07-13 11:31:46 +0330

    Update mian.py

--- image coverter exercise/mian.py
+++ image coverter exercise/mian.py
@@ -3,24 +3,28 @@ from ImageFactory import ImageFactory
 def main():
     factory = ImageFactory()
 
-    # Create and calculate size of BMP image
-    bmp_image = factory.create_image('BMP', 640, 480, 0.5)
+    # ایجاد و محاسبه اندازه تصویر BMP
+    bmp_image = factory.create_image('BMP', 'path/to/your/image.bmp', 800, 600, 0.7)
     print(f"BMP Image Size: {bmp_image.calculate_size()} bytes.")
 
-    # Create and calculate size of PNG image
-    png_image = factory.create_image('PNG', 800, 600, 80, False)
+    # ایجاد و محاسبه اندازه تصویر PNG
+    png_image = factory.create_image('PNG', 'path/to/your/image.png', 1024, 768, 100, True)
     print(f"PNG Image Size: {png_image.calculate_size()} bytes.")
 
-    # Create and calculate size of TGA image
-    tga_image = factory.create_image('TGA', 400, 300, True, 7)
+    # ایجاد و محاسبه اندازه تصویر TGA
+    tga_image = factory.create_image('TGA', 'path/to/your/image.tga', 1280, 720, True, 5)
     print(f"TGA Image Size: {tga_image.calculate_size()} bytes.")
 
-    # Create and calculate size of TIFF image
-    tiff_image = factory.create_image('TIFF', 1024, 768, 300, False, {'Author': 'Mohammad Hossein Minaee', 'Professor': 'Dr. Pourya Khanzadi'})
-    print(f"TIFF Image Size: {tiff_image.calculate_size()} bytes.")
+    # ایجاد و محاسبه اندازه تصویر TIFF با لایه‌ها
+    tiff_image_true = factory.create_image('TIFF', 'path/to/your/image.tiff', 1920, 1080, 300, True, {'Author': 'Mohammad Hossein Minaee', 'Professor': 'Dr. Pourya Khanzadi'})
+    print(f"TIFF Image Size with Layers Saved: {tiff_image_true.calculate_size()} bytes.")
 
-    # Convert TIFF image to BMP format and calculate size
-    converted_image = factory.convert_image(tiff_image, 'BMP', 0.2)
+    # ایجاد و محاسبه اندازه تصویر TIFF بدون لایه‌ها
+    tiff_image_false = factory.create_image('TIFF', 'path/to/your/image.tiff', 1920, 1080, 300, False, {'Author': 'Mohammad Hossein Minaee', 'Professor': 'Dr. Pourya Khanzadi'})
+    print(f"TIFF Image Size without Layers Saved: {tiff_image_false.calculate_size()} bytes.")
+
+    # تبدیل تصویر TIFF به فرمت BMP و محاسبه اندازه
+    converted_image = factory.convert_image(tiff_image_true, 'BMP', 'path/to/your/converted_image.bmp', 0.3)
     print(f"Converted TIFF to BMP Size: {converted_image.calculate_size()} bytes.")
 
 if __name__ == "__main__":
